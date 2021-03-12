535b93aae1e9d7ffd6dae480d60ba38ce155d0be
Tsogoeva <tdagger@mail.ru>
2021-03-12 20:22:42 +0300

    feat: solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const reverseNum = String(n)
+        .split('')
+        .filter(element => element !== '-')
+        .reverse()
+        .join('');
+    return Number(reverseNum);
 }
